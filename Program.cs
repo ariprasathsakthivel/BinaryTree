09f5542979106f9771321356725b0cff7986147d
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-26 14:53:07 +0530

    [Ariprasath] added: initialising a binary tree with a number 56. After that adding two numbers, 30 and 70

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace BinaryTree
+namespace BinarySearchTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Binary Tree program");
+            Console.WriteLine("Welcome to Binary Search Tree");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            Console.ReadKey();
         }
     }
 }
